6c5e6ed2ea8a111aec6b4cfb4d8c5ae51af8a525
Nwel <nweldev@users.noreply.github.com>
2023-09-06 02:06:04 +0200

    chore: add sass for documents
    use scss for proper "document" styling (anticipating markdown) via
    wrapper module (best approach to isolate it completly from other
    parts of the application, as once again React revinvented the wheel
    making any attempt to use a shadow DOM kind of a hack)

--- src/app/[lang]/countries/[code]/page.tsx
+++ src/app/[lang]/countries/[code]/page.tsx
@@ -2,20 +2,23 @@ import { Locale } from '@/i18n-config';
 import { getDictionary } from '@/get-dictionnary';
 import { getBaseUrl } from '@/app/shared/utils/getBaseUrl';
 
-  const fetchCountry = async (code: string) => {
-    const res = await fetch(`${getBaseUrl()}/api/countries/${code}?synthetic}`, { cache: 'force-cache' });
-    const { data } = await res.json();
-    
-    return data;
-  };
+import styles from '../../document.module.scss';
+import classNames from 'classnames';
 
-export default async function Countries({ params: { lang, code: countryCode } }: { params: { lang: Locale, code: string } }) {
-  const dictionary = await getDictionary(lang);
-  const country  = await fetchCountry(countryCode);
+const fetchCountry = async (code: string) => {
+  const res = await fetch(`${getBaseUrl()}/api/countries/${code}?synthetic=true}`, { cache: 'force-cache' });
+  const { data } = await res.json();
+
+  return data;
+};
+
+export default async function Countries({ params: { lang, code: countryCode } }: { params: { lang: Locale; code: string } }) {
+  const dictionary = (await getDictionary(lang)).countries.details;
+  const country = await fetchCountry(countryCode);
 
   return (
-    <>
-    <h1>{country.name[lang] || country.name.vo}</h1>
-    </>
+    <div className={classNames('min-h-[--h-full-content]', styles.document)}>
+      <h1>{country.name[lang] || country.name.vo}</h1>
+    </div>
   );
 }
